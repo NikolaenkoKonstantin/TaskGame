d706eb1c43ca253543e8a25856c4096a1a6cebfc
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-09-23 15:37:43 +0300

    feat: add level and update parameters

--- src/main/java/org/example/characters/Creature.java
+++ src/main/java/org/example/characters/Creature.java
@@ -3,13 +3,43 @@ package org.example.characters;
 import java.util.Random;
 
 public class Creature {
+    private int level = 1;
     private int attack; // 1 - 30
     private int protection; // 1 - 30
     protected int tempHealth; // 0 - N
     protected int maxHealth;
-    private final int[] damage; // N - M
+    private int[] damage; // N - M
+    private boolean isDied = false;
 
     Random random = new Random();
+
+
+    private boolean isDied(){
+        return isDied;
+    }
+
+
+    private void levelUp(Creature creature){
+        if(creature.isDied()) {
+            this.level++;
+            improveParameters();
+        }
+    }
+
+
+    private void improveParameters() {
+        this.attack++;
+        this.protection++;
+        healthChange();
+        this.damage = new int[]{damage[0] + 1, damage[1] + 1};
+    }
+
+
+    private void healthChange() {
+        double percentTempHealth = (double) tempHealth / maxHealth;
+        this.maxHealth += 5;
+        this.tempHealth = (int) (this.maxHealth * percentTempHealth);
+    }
 
 
     public void takingDamage(int[] damage){
@@ -33,6 +63,7 @@ public class Creature {
             if(random.nextInt(7) >= 5 ){
                 System.out.println("Successful strike");
                 creature.takingDamage(damage);
+                levelUp(creature);
                 break;
             }
         }
@@ -41,6 +72,7 @@ public class Creature {
 
     public void die(){
         if(tempHealth == 0){
+            this.isDied = true;
             System.out.println("The creature died");
         }
     }
@@ -92,5 +124,25 @@ public class Creature {
 
     public void setMaxHealth(int maxHealth) {
         this.maxHealth = maxHealth;
+    }
+
+
+    public int getLevel() {
+        return level;
+    }
+
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+
+    public int[] getDamage() {
+        return damage;
+    }
+
+
+    public void setDamage(int[] damage) {
+        this.damage = damage;
     }
 }
