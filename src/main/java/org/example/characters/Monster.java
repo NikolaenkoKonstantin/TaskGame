d87b2e594d66b56c9996873ad27e583685aa5ab8
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-09-23 13:40:52 +0300

    feat: added "Monster" class

---
+++ src/main/java/org/example/characters/Monster.java
@@ -0,0 +1,7 @@
+package org.example.characters;
+
+public class Monster extends Creature{
+    public Monster(int attack, int protection, int health, int[] damage) {
+        super(attack, protection, health, damage);
+    }
+}
