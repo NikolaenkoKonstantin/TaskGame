2a1630c8c0e796a65316c67ecdfa694ba4b80533
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-09-23 15:11:36 +0300

    fix: edit access modifications "Creature" class

--- src/main/java/org/example/characters/Creature.java
+++ src/main/java/org/example/characters/Creature.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class Creature {
     private int attack; // 1 - 30
     private int protection; // 1 - 30
-    private int health; // 0 - N
+    protected int tempHealth; // 0 - N
+    protected int maxHealth;
     private final int[] damage; // N - M
 
     Random random = new Random();
@@ -14,15 +15,16 @@ public class Creature {
     public void takingDamage(int[] damage){
         int DamageDealt = random.nextInt(damage[0], damage[1]);
 
-        if(health > DamageDealt){
-            health -= DamageDealt;
+        if(tempHealth > DamageDealt){
+            tempHealth -= DamageDealt;
         }else{
-            health = 0;
+            tempHealth = 0;
         }
 
         System.out.println("Damage taken " + DamageDealt);
         die();
     }
+
 
     public void hit(Creature creature){
         int attackModifier = attack - creature.getProtection() + 1;
@@ -36,40 +38,59 @@ public class Creature {
         }
     }
 
+
     public void die(){
-        if(health == 0){
+        if(tempHealth == 0){
             System.out.println("The creature died");
         }
     }
 
+
     public Creature(int attack, int protection, int health, int[] damage) {
         this.attack = attack;
         this.protection = protection;
-        this.health = health;
+        this.tempHealth = health;
+        this.maxHealth = health;
         this.damage = damage;
     }
+
 
     public int getAttack() {
         return attack;
     }
 
+
     public void setAttack(int attack) {
         this.attack = attack;
     }
+
 
     public int getProtection() {
         return protection;
     }
 
+
     public void setProtection(int protection) {
         this.protection = protection;
     }
 
-    public int getHealth() {
-        return health;
+
+    public int getTempHealth() {
+        return tempHealth;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
+
+    public void setTempHealth(int tempHealth) {
+        this.tempHealth = tempHealth;
+    }
+
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
     }
 }
